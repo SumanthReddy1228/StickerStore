37114ce2e7f0603602dc531187af4db6b707b44b
sumanth <sumanth0128@gmail.com>
2026-01-24 22:51:29 +0530

    JPA Auditing

--- src/main/java/com/eazybytes/eazystore/entity/BaseEntity.java
+++ src/main/java/com/eazybytes/eazystore/entity/BaseEntity.java
@@ -1,4 +1,44 @@
 package com.eazybytes.eazystore.entity;
 
-public class BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity{
+
+    @Column(name = "CREATED_AT", nullable = false,updatable = false)
+    @CreatedDate
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @CreatedBy
+    @Column(name = "CREATED_BY", nullable = false, length = 20,updatable = false)
+    private String createdBy;
+
+    @LastModifiedDate
+    @UpdateTimestamp
+    @Column(name = "UPDATED_AT", insertable = false)
+    private Instant updatedAt;
+
+
+    @Column(name = "UPDATED_BY", length = 20,insertable = false)
+    @LastModifiedBy
+    private String updatedBy;
+
 }
